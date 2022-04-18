04f51945129f9734850224e19563913b74bd4284
David <depch47@gmail.com>
2022-04-18 14:20:25 -0500

    exercise 3 done

---
+++ firstMonth/javaWorkshop/src/exercises/one/ExerciseThree.java
@@ -0,0 +1,25 @@
+package exercises.one;
+
+
+import static exercises.one.ExerciseOne.getNumber;
+
+/*
+* Haz una aplicación que calcule el área de un círculo(pi*R2).
+* El radio se pedirá por teclado (recuerda pasar de String a double con Double.parseDouble).
+* Usa la constante PI y el método pow de Math.*/
+public class ExerciseThree {
+    public static void main(String[] args) {
+        try{
+            double radius = getNumber();
+            System.out.println("Area is: " + calculateCircleArea(radius));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static double calculateCircleArea(double radius) {
+        double diameter = radius * 2;
+        double  PI = Math.PI;
+        return PI * Math.pow(diameter/2, 2);
+    }
+}
