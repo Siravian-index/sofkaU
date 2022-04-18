9f7f5de060a792c34973605f19997eaf99f2f557
David <depch47@gmail.com>
2022-04-18 14:36:23 -0500

    exercise 7 done

---
+++ firstMonth/javaWorkshop/src/exercises/ExerciseSeven.java
@@ -0,0 +1,25 @@
+package exercises;
+
+import static exercises.ExerciseOne.getNumber;
+
+/*
+ * Lee un número por teclado y comprueba que este numero es mayor o igual que cero,
+ * si no lo es lo volverá a pedir (do while), después muestra ese número por consola.*/
+public class ExerciseSeven {
+    public static void main(String[] args) {
+        askNumber();
+    }
+
+    public static void askNumber() {
+        double number;
+        try {
+            do {
+                 number = getNumber();
+            } while (number < 0);
+            System.out.println(number);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
