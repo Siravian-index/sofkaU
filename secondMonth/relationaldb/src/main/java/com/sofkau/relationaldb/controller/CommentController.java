55806bdb2165b4579f5d71ddc4942751cb75b825
David <depch47@gmail.com>
2022-04-30 15:41:22 -0500

    changed @Controller for @RestController, it should work now

--- secondMonth/relationaldb/src/main/java/com/sofkau/relationaldb/controller/CommentController.java
+++ secondMonth/relationaldb/src/main/java/com/sofkau/relationaldb/controller/CommentController.java
@@ -4,13 +4,9 @@ import com.sofkau.relationaldb.entity.Comment;
 import com.sofkau.relationaldb.entity.Post;
 import com.sofkau.relationaldb.service.CommentServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/comments")
 public class CommentController {
     private final CommentServiceImplementation commentService;
--- secondMonth/relationaldb/src/main/java/com/sofkau/relationaldb/controller/PostController.java
+++ secondMonth/relationaldb/src/main/java/com/sofkau/relationaldb/controller/PostController.java
@@ -3,12 +3,11 @@ package com.sofkau.relationaldb.controller;
 import com.sofkau.relationaldb.entity.Post;
 import com.sofkau.relationaldb.service.PostServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("api/v1/posts")
 public class PostController {
 
